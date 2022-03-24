4f7abed9ee3697161f2d69843c8b34eb96745567
cabecada <vijaykumarjain.github@gmail.com>
2022-03-24 22:02:43 +0530

    use sql functions instead of plpgsql

--- create_aggregate.sql
+++ create_aggregate.sql
@@ -1,68 +1,21 @@
-drop aggregate if exists min_to_max(int);
-drop function if exists mintomax_sfunc(point, int);
-drop function if exists mintomax_finalfunc(point);
+drop aggregate if exists minmax(anyelement);
+drop function if exists  minmax_sf(anyarray, anyelement);
+drop function if exists  minmax_final(anyarray);
 
-create or replace function mintomax_sfunc(minmax point, el int) returns point immutable language plpgsql as $$
-declare current_min int; current_max int;
-begin
-	if minmax is null then
-		return point(el, el);
-	end if;
-
-	if minmax[0] is not null and minmax[0] > el then
-		current_min := el;
-	else
-		current_min := minmax[0];
-	end if;
-
-	if minmax[0] is not null and minmax[1] < el then
-		current_max := el;
-	else
-		current_max := minmax[1];
-	end if;
-
-	return point(current_min, current_max);
-
-end;
+CREATE OR REPLACE FUNCTION minmax_sf(anyarray, anyelement)
+RETURNS anyarray LANGUAGE sql IMMUTABLE STRICT AS $$
+        SELECT ARRAY[least($1[1],$2), greatest($1[2], $2)];
 $$;
 
-create or replace function mintomax_finalfunc(minmax point) returns text immutable language plpgsql as $$
-begin
-	return format('%s -> %s', minmax[0], minmax[1]);
-end;
+CREATE OR REPLACE FUNCTION minmax_final(anyarray)
+RETURNS text LANGUAGE sql IMMUTABLE STRICT AS $$
+	SELECT case when $1 is NULL then NULL else $1[1]::text || ' -> ' || $1[2]::text END;
 $$;
 
-create or replace aggregate min_to_max(int)
-(
-        sfunc = mintomax_sfunc,
-        stype = point,
-        finalfunc = mintomax_finalfunc
+CREATE AGGREGATE minmax(anyelement) (
+        sfunc     = minmax_sf,
+        stype     = anyarray,
+        finalfunc = minmax_final,
+        initcond = '{NULL, NULL}'
 );
-
-
-/*
-example=# select min_to_max(id) from (values(NULL::int),(NULL::int),(NULL::int),(NULL::int)) t(id);
- min_to_max
-------------
-  ->
-(1 row)
-
-example=# select min_to_max(id) from (values(NULL::int),(NULL::int),(NULL::int),(1::int)) t(id);
- min_to_max
-------------
- 1 -> 1
-(1 row)
-
-example=# select min_to_max(id) from (values(NULL::int),(NULL::int),(2::int),(1::int)) t(id);
- min_to_max
-------------
- 1 -> 2
-(1 row)
-
-example=# select min_to_max(id) from (values(NULL::int),(NULL::int),(-2::int),(1::int)) t(id);
- min_to_max
-------------
- -2 -> 1
-(1 row)
-*/
 
